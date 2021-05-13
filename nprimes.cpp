615e1d8e458e05f15584331278c0953892b80bd2
palaparthivennela <69576559+palaparthivennela@users.noreply.github.com>
2021-05-13 10:00:32 +0530

    Create nprimes.cpp

---
+++ nprimes.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0,i,j;
+    cout<<"enter a number";
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        for(j=2;j<=sqrt(i)+1;j++)
+        {
+            if (i%j==0)
+            {
+                count=1;
+                break;
+            }
+        }
+        if (count==0)
+        {
+            cout<<i<<" ";
+        }
+        else
+        {
+            count=0;
+        }
+    }
+return 0;
+}
